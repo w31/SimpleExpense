ced1e93ee671e9d742c8016d26fee2522b45ec8e
Wei <w31@users.noreply.github.com>
2014-08-17 16:41:03 -0500

    Add basic navigation.

--- SimpleExpense.UI/assets/js/app.js
+++ SimpleExpense.UI/assets/js/app.js
@@ -1,1 +1,18 @@
-﻿var app = angular.module('ExpenseApp', []);
+﻿var app = angular.module('expenseApp', []);
+
+app.controller('AppController', function () {
+    this.expenses = [];
+    this.categories = [];
+});
+
+app.controller('NavController', function () {
+    this.tab = 1;
+
+    this.setTab = function (tab) {
+        this.tab = tab;
+    }
+
+    this.isSet = function (tab) {
+        return this.tab === tab;
+    };
+});
