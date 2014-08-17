7553ab1bcc67fac99d82cce10555180953b04ce9
Wei <w31@users.noreply.github.com>
2014-08-17 18:05:08 -0500

    Display expenses and categories.

--- SimpleExpense.UI/assets/js/app.js
+++ SimpleExpense.UI/assets/js/app.js
@@ -1,9 +1,16 @@
 ﻿var app = angular.module('expenseApp', []);
 
-app.controller('AppController', function () {
-    this.expenses = [];
-    this.categories = [];
-});
+app.controller('AppController', ['expenseService', function (expenseService) {
+    var thisApp = this;
+
+    expenseService.getCategories().success(function (data) {
+        thisApp.categories = data;
+    });
+
+    expenseService.getExpenses().success(function (data) {
+        thisApp.expenses = data;
+    });
+}]);
 
 app.controller('NavController', function () {
     this.tab = 1;
@@ -16,3 +23,32 @@ app.controller('NavController', function () {
         return this.tab === tab;
     };
 });
+
+app.controller('CategoryController', ['expenseService', function (expenseService) {
+    this.category = {};
+
+    this.addCategory = function () {
+        expenseService.addCategory(this.category);
+        this.category = {};
+    };
+}]);
+
+app.service('expenseService', ['$http', function ($http) {
+    var endpoint = "http://localhost:11171/api";
+
+    this.getCategories = function () {
+        return $http.get(endpoint + '/category');
+    };
+
+    this.addCategory = function (data) {
+        return $http.post(endpoint + '/category', data);
+    }
+
+    this.getExpenses = function () {
+        return $http.get(endpoint + '/expense');
+    };
+
+    this.addExpense = function (data) {
+        return $http.post(endpoint + '/expense', data);
+    };
+}]);
